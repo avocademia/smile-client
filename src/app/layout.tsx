a042e3eceeece318d65a1ef008272f49f9498a1f
Jotham Jami <jothamoj@gmail.com>
2025-10-13 15:35:15 +0300

    font utilixzation update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,22 @@
 import type { Metadata } from "next";
 import './globals.scss'
+import { Quicksand,Rancho } from 'next/font/google'
+
+const quicksand = Quicksand({
+  subsets: ['latin'],
+  weight: ['400', '500', '700'],
+  variable: '--font-quicksand'
+})
+
+const rancho = Rancho({
+  subsets: ['latin'],
+  weight: ['400'],
+  variable: '--font-rancho'
+})
 
 export const metadata: Metadata = {
-  title: "Yana Green Ribbon",
-  description: "Mentally healthy people, Mentally healthy community",
+  title: "SMILE",
+  description: "Supporting Minors to Improve Life",
 };
 
 export default function RootLayout({
@@ -12,7 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${quicksand.variable} ${rancho.variable}`}>
       <body>
         {children}
       </body>
