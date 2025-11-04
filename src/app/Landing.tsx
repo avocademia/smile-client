ffd5aa633f062937486bed75119684e17f8f9860
Jotham Jami <jothamoj@gmail.com>
2025-11-04 10:22:07 +0300

    minor fix

--- src/app/Landing.tsx
+++ src/app/Landing.tsx
@@ -30,7 +30,6 @@ const AnimatedItem = ({ children, delay = 0, index, onMouseEnter, onClick }: pro
         initial={{ scale: 0.7, opacity: 0 }}
         animate={inView ? { scale: 1, opacity: 1 } : { scale: 0.7, opacity: 0 }}
         transition={{ duration: 0.2, delay }}
-        style={{ marginBottom: '1rem', cursor: 'pointer' }}
       >
         {children}
       </motion.div>
